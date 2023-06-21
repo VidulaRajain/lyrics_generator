0187f64186602a84565c475a8a298927b1ec90fa
VidulaRajain <75062162+VidulaRajain@users.noreply.github.com>
2023-06-21 15:01:07 +0530

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -8,12 +8,12 @@ st.text('In the style of Taylor Swift')
 
 song_input = st.text_area('Enter the starting of your song', help='Keep it short: 15-20 words')
 
-part  = st.radio('Chose with part of the song the input represents:', ['Verse 1', 'Intro'])
+part  = st.radio('Chose which part of the song the input represents:', ['Verse 1', 'Intro'])
 
 generate = st.button('Generate Lyrics')
 
 if generate:
-    while st.spinner('Generating Lyrics for your song....'):
+    with st.spinner('Generating Lyrics for your song....'):
         rest_of_the_song = LyricsGenerator().run(
             key=st.secrets['OPENAI_API_KEY'], 
             input_lyrics=song_input, 
